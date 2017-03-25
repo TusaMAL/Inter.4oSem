98291a8fbfcabcec82c6e7db8803068040081051
Felipe Carlos Dos Santos <felipe.carlos1504@gmail.com>
2017-03-25 17:37:14 -0300

    Adicionado Classes Pessoa e Usuario
    falta 3 atributos da classe usuário

---
+++ DisciplineTeam.Area52/DisciplineTeam.Area52.Web/Models/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DisciplineTeam.Area52.Web.Models
+{
+    public class Pessoa
+    {
+        public int IdPessoa { get; set; }
+
+        [Required]
+        public string nome { get; set; }
+
+        [Required]
+        public string nick { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string senha { get; set; }
+
+        public int status { get; set; }
+    }
+}
---
+++ DisciplineTeam.Area52/DisciplineTeam.Area52.Web/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DisciplineTeam.Area52.Web.Models
+{
+    public class Usuario
+    {
+        [Required]
+        public string datanasc { get; set; }
+
+        public string descricao { get; set; }
+
+        public string imagem { get; set; }
+    }
+}
