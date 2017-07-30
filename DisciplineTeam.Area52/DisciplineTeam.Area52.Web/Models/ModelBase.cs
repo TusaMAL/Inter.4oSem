fc9f41680b8ee66f694046d101c0f6e6871a80f3
Felipe Carlos Dos Santos <felipe.carlos1504@gmail.com>
2017-07-30 17:06:56 -0300

    Ave

--- DisciplineTeam.Area52/DisciplineTeam.Area52.Web/Models/ModelBase.cs
+++ DisciplineTeam.Area52/DisciplineTeam.Area52.Web/Models/ModelBase.cs
@@ -17,7 +17,7 @@ namespace DisciplineTeam.Area52.Web.Models
             //    " Integrated Security = true;" +
             //      "User Id = sa; Password = dba";
 
-            string strConn = "Server=tcp:area52.database.windows.net,1433;Initial Catalog=BDarea52;Persist Security Info=False;User ID=felipe;Password=m9p13ku2jM;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            
 
             connection = new SqlConnection(strConn);
 
